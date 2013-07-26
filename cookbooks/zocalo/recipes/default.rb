c3fd61808a6091bfd574dcea9cf28069c45581df
Larry Gilbert <larry@L2G.to>
2013-07-26 08:07:54 -0700

    First working recipe for Zocalo

--- cookbooks/zocalo/recipes/default.rb
+++ cookbooks/zocalo/recipes/default.rb
@@ -1,6 +1,20 @@
+# vim: ts=2:sw=2:et:
+
 #
 # Cookbook Name:: zocalo
 # Recipe:: default
 #
 # Copyright 2013 Lawrence Leonard Gilbert
 #
+
+include_recipe 'java'
+include_recipe 'subversion::client'
+
+application 'zocalo' do
+  path         '/opt/zocalo'
+  owner        'vagrant'
+  group        'users'
+  scm_provider Chef::Provider::Subversion
+  repository   'http://svn.code.sf.net/p/zocalo/code/tags/2011.1'
+end
+
