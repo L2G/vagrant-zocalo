29e475561a7648d6e0cc373a7092ec7e4b762c6c
Larry Gilbert <larry@L2G.to>
2013-07-26 10:25:35 -0700

    Simpler recipe that downloads a Zocalo tarball instead of building

--- cookbooks/zocalo/recipes/default.rb
+++ cookbooks/zocalo/recipes/default.rb
@@ -8,13 +8,19 @@
 #
 
 include_recipe 'java'
-include_recipe 'subversion::client'
 
-application 'zocalo' do
-  path         '/opt/zocalo'
-  owner        'vagrant'
+tarball_url = 'http://downloads.sourceforge.net/project/zocalo/zocalo-2011.2/zocalo-PM-2011-2-bin.tar.gz'
+
+directory '/opt/zocalo' do
+  owner 'vagrant'
+  group 'users'
+end
+
+tar_extract tarball_url do
+  target_dir   '/opt'
+  user         'vagrant'
   group        'users'
-  scm_provider Chef::Provider::Subversion
-  repository   'http://svn.code.sf.net/p/zocalo/code/tags/2011.1'
+  download_dir '/vagrant'
+  not_if       { ::File.exist?('/opt/zocalo/bin/zocalo.sh') }
 end
 
